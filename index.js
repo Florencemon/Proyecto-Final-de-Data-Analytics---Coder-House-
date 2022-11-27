d481ad3bb15a05b2bb69c7cf90efbe5774f16aa6
Florencia Chirino <101209638+Florencemon@users.noreply.github.com>
2022-11-27 17:56:22 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+### Hi there 👋 
+
+                                  
+                                  
+```
+javascript
+
+Informe_BI_publicado_en_la_WEB:
+(https://app.powerbi.com/view?r=eyJrIjoiMjc4OWVmODctNmVkMi00M2I3LWE5MTUtMmFiN2MwNGU0OWNhIiwidCI6IjFhZDg2Mzc0LTA5YWUtNDBjMS04ZWVmLTY2MmIyMDdhOTNmZiJ9&pageName=ReportSectiona71177e6190340b3a0b5)
+
+
+fuente_Quicksand_de_Google_Fonts: (https://fonts.google.com/specimen/Quicksand)
+
+```
